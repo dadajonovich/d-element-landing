d402128e3d4d33f8cc95307b958488e1d7663b29
Norbaev <timnrbv@gmail.com>
2023-10-22 20:36:01 +0500

    burger

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 const menuItems = ['Home', 'About us', 'Category'];
 
-class App {}
+class App {
+  constructor() {
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', () =>
+      burger.classList.toggle('burger_active'),
+    );
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => new App());
